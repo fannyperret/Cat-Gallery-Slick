82dcc798590b6ae35a7056c33e6c17da943360c8
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-14 14:50:23 +0200

    change some items pictures and script js :cat:

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.gallery').slick( {
+    dots: true,
+    arrows: false,
+    speed: 1000,
+
+    slidesToShow: 1,
+
+    centerMode: true,
+    variableWidth: true,
+
+    autoplay: true,
+    autoplaySpeed: 5000,
+
+
+  });
+});
