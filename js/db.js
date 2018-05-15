640a73a81174095c5a127daf083b18f48a0c665e
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-15 10:56:04 +0200

    add js file and css file for loading bar and adjust some scripts javascript to make a db in js for cat :cat:

---
+++ js/db.js
@@ -0,0 +1,10 @@
+const db = {
+  'images' : [
+    { type: 'image', src: "assets/img/cat1.jpg" },
+    { type: 'image', src: "assets/img/cat2.jpg" },
+    { type: 'text', content:"test test <i>test</i>" },
+    { type: 'image', src: "assets/img/cat2.jpg" },
+    { type: 'image', src: "assets/img/cat5.jpg" },
+    { type: 'image', src: "assets/img/cat8.jpg" }
+  ]
+}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-  $('#slider').slick( {
 
+  for( let i in db.images){
+    if(db.images[i].type == "image"){
+      $('#slider').append($('<div class="item"><img class="img-fluid" src="' + db.images[i].src + '" /></div>'));
+    }
+    else if(db.images[i].type == "text"){
+      $('#slider').append($('<div class="item"><p>' + db.images[i].content + '</p></div>'));
+    }
+  }
+
+  $('#slider').slick( {
     infinite: true,
     autoplay: true,
     autoplaySpeed: 5000,
-    adaptiveHeight: true,
-
     fade: true
-
   })
 
 })
