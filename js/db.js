826905a0163b656fda0be37b7086e8de8ecea833
Fanny Perret <fannyperretlombard@gmail.com>
2018-05-15 12:04:22 +0200

    Atomic Kitten last commit? :cat:

--- js/db.js
+++ js/db.js
@@ -1,10 +1,21 @@
 const db = {
   'images' : [
+    { type: 'text', content:"Cute cat! Purr Purr" },
     { type: 'image', src: "assets/img/cat1.jpg" },
     { type: 'image', src: "assets/img/cat2.jpg" },
-    { type: 'text', content:"test test <i>test</i>" },
-    { type: 'image', src: "assets/img/cat2.jpg" },
+    { type: 'text', content:"Cute cat! Purr Purr" },
+    { type: 'image', src: "assets/img/cat4.jpg" },
     { type: 'image', src: "assets/img/cat5.jpg" },
-    { type: 'image', src: "assets/img/cat8.jpg" }
+    { type: 'image', src: "assets/img/cat6.jpg" },
+    // { type: 'textImage', src: "assets/img/cat7.jpg" },
+    { type: 'image', src: "assets/img/cat8.jpg" },
+    { type: 'image', src: "assets/img/cat9.jpg" },
+    { type: 'image', src: "assets/img/cat10.jpg" },
+    // { type: 'textImage', src: "assets/img/cat11.jpg" },
+    { type: 'image', src: "assets/img/cat12.jpg" },
+    { type: 'image', src: "assets/img/cat13.jpg" },
+    { type: 'image', src: "assets/img/cat14.jpg" },
+    { type: 'image', src: "assets/img/cat15.jpg" },
+    // { type: 'textImage', src: "assets/img/cat16.jpg" }
   ]
 }
